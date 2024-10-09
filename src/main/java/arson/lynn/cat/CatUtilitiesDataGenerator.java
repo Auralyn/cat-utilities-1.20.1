2ef9b1903bf0c620896abc368edc6d9c5ed7e9d4
Auralyn <auralynnnnn@yahoo.com>
2024-10-09 12:13:17 +0200

    First Commit

---
+++ src/main/java/arson/lynn/cat/CatUtilitiesDataGenerator.java
@@ -0,0 +1,11 @@
+package arson.lynn.cat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CatUtilitiesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
